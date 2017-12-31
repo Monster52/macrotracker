54c532d8993b6d83b11cd30c0a8ae8c36abff4b2
Ross Waguespack <rosswaguespack@gmail.com>
2017-12-31 13:39:45 -0600

    fix error when no tracker is created

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,8 @@ class ProfilesController < ApplicationController
   def index
   	@user = current_user
     @goal = current_user.goal
-
-    if @user.trackers.last.updated_at.to_date == Date.today
+# opton if not created
+    if @user.trackers && @user.trackers.last.updated_at.to_date == Date.today
   	   macro = MacroService.new({protein: @user.trackers.pluck(:protein).inject(:+),
   			   											carbohydrate: @user.trackers.pluck(:carbohydrate).inject(:+), 
   		  												fat: @user.trackers.pluck(:fat).inject(:+)})
